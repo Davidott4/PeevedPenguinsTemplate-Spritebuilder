99859837cf126743f147b0a2ed57e2f283a552c5
dott <dott@mail.ccsf.edu>
2014-06-25 10:49:25 -0700

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/AppDelegate.m
+++ PeevedPenguins.spritebuilder/Source/AppDelegate.m
@@ -55,9 +55,9 @@
     [self setupCocos2dWithOptions:cocos2dSetup];
     
           
-    //      [CCBReader load:@"Penguin"];
+    [CCBReader load:@"Penguin"];
     
-    [CCBReader load:@"Seal"];
+    //[CCBReader load:@"Seal"];
 
     
     return YES;
