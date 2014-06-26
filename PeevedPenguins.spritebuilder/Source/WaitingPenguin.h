146795aaf066f7d9c2815bab644603d901c867c5
dott <dott@mail.ccsf.edu>
2014-06-26 12:04:25 -0700

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/WaitingPenguin.h
@@ -0,0 +1,16 @@
+//
+//  WaitingPenguin.h
+//  PeevedPenguins
+//
+//  Created by David Ott on 6/26/14.
+//  Copyright 2014 Apportable. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+@interface WaitingPenguin : CCSprite {
+    
+}
+
+@end
